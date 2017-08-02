eeaf95934835e198f2b4c2b1f2a12a7c352c4e77
Cam Macfarlane <cammac93@gmail.com>
2017-08-01 20:00:59 -0700

    removed unnecessary imports

--- Project/cam_learning.py
+++ Project/cam_learning.py
@@ -13,15 +13,6 @@ from sklearn.neural_network import MLPClassifier
 import pandas
 import matplotlib.pyplot as plt
 from sklearn import model_selection
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
 
 # SERVER CONFIG:
 # ///////////////////////////////////////////////////////////////////////////////////////////
